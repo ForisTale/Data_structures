dcc3e12c49d251f292c5b9e6711b61c99677dcaf
Foris <foris4@gmail.com>
2019-05-15 14:40:19 +0100

    Delete redundant tests.

--- test/test_unidirectional_linked_list.py
+++ test/test_unidirectional_linked_list.py
@@ -3,22 +3,6 @@ from node import Node
 import pytest
 
 
-def test_default_values():
-    linked_list = ULinkedList()
-    assert linked_list.head_node is None
-
-
-def test_can_start_with_node():
-    node = Node("test")
-    linked_list = ULinkedList(node)
-    assert linked_list.head_node == node
-
-
-def test_has_get_head_node():
-    linked_list = ULinkedList()
-    linked_list.get_head_node()
-
-
 def test_add_node():
     node = Node(1)
     linked_list = ULinkedList()
@@ -29,18 +13,6 @@ def test_add_node():
     other_node = Node(2)
     linked_list.add_node(other_node)
     assert linked_list.head_node.get_link_node() == node
-
-
-def test_can_iter_linked_list():
-    node_1 = Node(1)
-    node_2 = Node(2)
-    node_3 = Node(3)
-
-    linked_list = ULinkedList(node_1)
-    linked_list.add_node(node_2)
-    linked_list.add_node(node_3)
-
-    assert [num for num in linked_list] == [3, 2, 1]
 
 
 @pytest.mark.parametrize("value, result", [
