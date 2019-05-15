acaa6b057ec8db0593a42c004ed1bcffd67f0b45
Foris <foris4@gmail.com>
2019-05-15 19:01:06 +0100

    Classes BidirectionalLinkedList and BidirectionalNode with tests are done with all tests passed.

--- bidirectional_linked_list.py
+++ bidirectional_linked_list.py
@@ -9,11 +9,27 @@ class BidirectionalLinkedList(LinkedList):
         self.get_head_node().set_prev_node(node_to_add)
         self.head_node = node_to_add
 
+    def remove_node(self, value):
+        if self.get_head_node().get_value() == value:
+            self.head_node = self.get_head_node().get_next_node()
+            return
+        current_node = self.get_head_node()
+        while current_node:
+            try:
+                if current_node.get_next_node().get_value() == value:
+                    next_node = current_node.get_next_node().get_next_node()
+                    next_node.set_prev_node(current_node)
+                    current_node.set_next_node(next_node)
+                    return
+            except AttributeError:
+                print("Error, can't find value in linked list to delete!")
+            current_node = current_node.get_next_node()
+
 
 class BidirectionalNode(Node):
     def __init__(self, value, next_node=None, prev_node=None):
+        super().__init__(value)
         self.link_node = next_node
-        self.value = value
         self.prev_node = prev_node
 
     def get_next_node(self):
--- test/test_bidirectional_linked_list.py
+++ test/test_bidirectional_linked_list.py
@@ -1,5 +1,6 @@
 from bidirectional_linked_list import BidirectionalLinkedList as BLinkedList
 from bidirectional_linked_list import BidirectionalNode as BNode
+import pytest
 
 
 def test_add_node():
@@ -14,6 +15,32 @@ def test_add_node():
 
     assert second_node.get_next_node() == node_1
     assert second_node.get_prev_node() == node_3
+
+
+@pytest.mark.parametrize("value, result, prev_node, next_node", [
+    (3, 1, "node_2", "None"),
+    (2, 1, "node_3", "None"),
+])
+def test_remove_node(value, result, prev_node, next_node):
+    node_1 = BNode(1)
+    node_2 = BNode(2)
+    node_3 = BNode(3)
+
+    linked_list = BLinkedList(node_1)
+    linked_list.add_node(node_2)
+    linked_list.add_node(node_3)
+
+    linked_list.remove_node(value)
+
+    assert linked_list.get_head_node().get_link_node().get_value() == result
+    assert linked_list.get_head_node().get_link_node().get_prev_node() == eval(prev_node)
+    assert linked_list.get_head_node().get_link_node().get_next_node() == eval(next_node)
+
+
+def test_get_error_message_when_try_remove_non_exist_value():
+    linked_list = BLinkedList(BNode(1))
+
+    assert linked_list.remove_node(5) == print("Error, can't find value in linked list to delete!")
 
 
 def test_bidirectional_node_has_basic_methods():
