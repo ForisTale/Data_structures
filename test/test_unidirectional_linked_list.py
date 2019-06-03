7605e9afe41897466b5cec167ba1fa56f4944543
Foris <foris4@gmail.com>
2019-06-03 09:55:13 +0100

    Refactoring.

--- test/test_unidirectional_linked_list.py
+++ test/test_unidirectional_linked_list.py
@@ -12,18 +12,19 @@ def test_add_node():
     assert linked_list.head_node.get_next_node().value == 1
 
 
-@pytest.mark.parametrize("value, result", [
-    (3, 1),
-    (2, 1),
+@pytest.mark.parametrize("value, value_list", [
+    (3, [2, 1]),
+    (2, [3, 1]),
 ])
-def test_remove_node(value, result):
+def test_remove_node(value, value_list):
     linked_list = ULinkedList(1)
     linked_list.add(2)
     linked_list.add(3)
 
     linked_list.remove_node(value)
+    ll_value_list = linked_list.traverse()
 
-    assert linked_list.get_head_node().get_next_node().get_value() == result
+    assert ll_value_list == value_list
 
 
 def test_get_error_message_when_try_remove_non_exist_value():
@@ -32,19 +33,20 @@ def test_get_error_message_when_try_remove_non_exist_value():
     assert linked_list.remove_node(5) == print("Error, can't find value in linked list to delete!")
 
 
-@pytest.mark.parametrize("value_to_pop, result, next_node_value", [
-    (2, 2, 3),
-    (1, 1, 3),
+@pytest.mark.parametrize("value_to_pop, result, value_list", [
+    (2, 2, [1, 3]),
+    (1, 1, [2, 3]),
 ])
-def test_pop_node(value_to_pop, result, next_node_value):
+def test_pop_node(value_to_pop, result, value_list):
     linked_list = ULinkedList(3)
     linked_list.add(2)
     linked_list.add(1)
 
     popped_node = linked_list.pop(value_to_pop)
+    ll_value_list = linked_list.traverse()
 
     assert popped_node.get_value() == result
-    assert linked_list.get_head_node().get_next_node().get_value() == next_node_value
+    assert ll_value_list == value_list
 
     linked_list.pop(3)
     assert linked_list.get_head_node().get_next_node() is None
