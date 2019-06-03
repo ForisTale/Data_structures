fcf7b5ff983ad9b9fdba67ddfff2989431a2834d
Foris <foris4@gmail.com>
2019-06-03 11:54:10 +0100

    Hash map can initialize.

---
+++ hash_map.py
@@ -0,0 +1,12 @@
+
+
+class HashMap:
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for _ in range(array_size)]
+
+    def assign(self, argument1, argument2):
+        pass
+
+    def retrieve(self, argument):
+        pass
--- test/test_hash_map.py
+++ test/test_hash_map.py
@@ -1,6 +1,13 @@
 from hash_map import HashMap
 
 
+def test_hash_map_initialization():
+    hash_map = HashMap(1)
+
+    assert hash_map.array_size == 1
+    assert len(hash_map.array) == 1
+
+
 def test_hash_map():
     hash_map = HashMap(2)
     hash_map.assign("key", "value")
