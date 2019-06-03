06e17822b1b779fe8815d0017aa5b8e3ffbf5fd4
Foris <foris4@gmail.com>
2019-06-03 19:37:43 +0100

    Preparing tests.

--- test/test_min_heap.py
+++ test/test_min_heap.py
@@ -1,5 +1,26 @@
 from min_heap import MinHeap
 
 
-def test_can_initialize():
+def test_min_heap():
     heap = MinHeap()
+    heap.add(1)
+    heap.add(2)
+
+    value = heap.retrieve()
+    assert value == 1
+
+
+def test_add():
+    heap = MinHeap()
+    heap.add(2)
+    heap.add(1)
+
+    assert heap.heap[0] == 1
+
+
+def test_heapify_up():
+    heap = MinHeap()
+    heap.heap = [None, 2, 3, 4, 1]
+    heap.heapify_up()
+
+    assert heap.heap == [None, 1, 2, 3, 4]
