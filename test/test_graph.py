e75075104517c06b2875a6b9d4cd10b97a28a847
Foris <foris4@gmail.com>
2019-06-06 09:04:33 +0100

    Change test add vertex that now will take value not vertex.

--- test/test_graph.py
+++ test/test_graph.py
@@ -2,28 +2,16 @@ from graph import Graph, Vertex
 import pytest
 
 
-vertex_1 = Vertex(1)
-vertex_2 = Vertex(2)
-vertex_3 = Vertex(3)
-vertex_4 = Vertex(4)
+def test_add_vertex(build_graph):
+    graph = build_graph
+    key_value = []
 
+    for key, item in graph.graph_dict.items():
+        key_value.append({key: item.value})
+    key_value.sort()
 
-@pytest.fixture(params=[True, False])
-def build_graph(request):
-    graph = Graph(request.param)
-    graph.add_vertex(vertex_1)
-    graph.add_vertex(vertex_2)
-    graph.add_vertex(vertex_3)
-    graph.add_vertex(vertex_4)
-
-    graph.add_edge(vertex_1, vertex_3)
-    graph.add_edge(vertex_3, vertex_2, 1)
-    yield graph
-
-    vertex_1.edges = {}
-    vertex_2.edges = {}
-    vertex_3.edges = {}
-    vertex_4.edges = {}
+    assert key_value == [{"vertex_1": "vertex_1"}, {"vertex_2": "vertex_2"},
+                         {"vertex_3": "vertex_3"}, {"vertex_4": "vertex_4"}, ]
 
 
 def test_is_directed(build_graph):
@@ -35,11 +23,6 @@ def test_is_directed(build_graph):
         cases_for_directed = {vertex_2: 1}
 
     assert vertex_3.edges == cases_for_directed
-
-
-def test_add_vertex(build_graph):
-    graph = build_graph
-    assert graph.graph_dict == {1: vertex_1, 2: vertex_2, 3: vertex_3, 4: vertex_4}
 
 
 def test_vertex():
@@ -54,3 +37,25 @@ def test_vertex():
     vertex_1.edges = {}
     vertex_1.add_edge(vertex_2, 1)
     assert vertex_1.edges == {vertex_2: 1}
+
+
+vertex_1 = Vertex(1)
+vertex_2 = Vertex(2)
+vertex_3 = Vertex(3)
+vertex_4 = Vertex(4)
+
+
+@pytest.fixture(params=[True, False])
+def build_graph(request):
+    graph = Graph(request.param)
+    graph.add_vertex("vertex_1")
+    graph.add_vertex("vertex_2")
+    graph.add_vertex("vertex_3")
+    graph.add_vertex("vertex_4")
+
+    graph.add_edge(vertex_1, vertex_3)
+    graph.add_edge(vertex_3, vertex_2, 1)
+    yield graph
+
+    for key, item in graph.graph_dict.items():
+        item.edges = {}
